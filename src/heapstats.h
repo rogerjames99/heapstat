c5ba812440b3fc4ed4f725cd96ca4e1e3c515d66
Roger James <roger@beardandsandals.co.uk>
2023-06-05 13:24:42 +0100

    Added a second timer member

--- src/heapstats.h
+++ src/heapstats.h
@@ -16,10 +16,12 @@ protected:
     static bool printHeapStats(void *);
     
     static CHeapStats* singleton;
+    
+    Timer<1> timer1; //This version gives a compiler warning.
 
-    // Timer<1> timer; // Use this version to see the compiler warning
     static Timer<1> timer; // Force the storage class to static - this supresses the
                             // compiler warning but still leaves the symbol undefined
+
 
 public:
     CHeapStats(CHeapStats &other) = delete;
